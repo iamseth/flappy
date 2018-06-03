349701d4c0e4f6e701a2a79058f4832cb891583f
Seth Miller <seth@sethmiller.me>
2018-06-03 14:51:53 -0500

    add nicer start menu

--- src/menu.lua
+++ src/menu.lua
@@ -14,7 +14,11 @@ end
 function Menu:draw(state) -- luacheck: ignore
     if (state == 'start') then
         love.graphics.setFont(love.graphics.newFont(40))
-        love.graphics.printf('Press space to begin.', 200, 200, 600, 'center')
+        love.graphics.printf('Press space to begin.', 200, 280, 600, 'center')
+        love.graphics.setLineWidth(6)
+        love.graphics.rectangle('line', 200, 200, 600, 300)
+        love.graphics.setLineWidth(4)
+        love.graphics.rectangle('line', 210, 210, 600 - 20, 300 - 20)
     elseif (state == 'pause') then
         love.graphics.setFont(love.graphics.newFont(40))
         love.graphics.printf('Game Paused', 200, 200, 600, 'center')
