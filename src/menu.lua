105c28f5c9b311e717337a326877a748c4c114ad
Seth Miller <seth@sethmiller.me>
2018-06-03 14:58:28 -0500

    cleanup menu

--- src/menu.lua
+++ src/menu.lua
@@ -5,25 +5,30 @@ Menu.__index = Menu
 
 
 function Menu:new()
-    local this = {}
+    local this = {
+        font = love.graphics.newFont(40),
+        color = { 255, 255, 255 }
+    }
     setmetatable(this, self)
     return this
 end
 
 
-function Menu:draw(state) -- luacheck: ignore
+function Menu:draw(state)
+
+    -- Set some defaults.
+    love.graphics.setFont(self.font)
+    love.graphics.setcolor(self.color)
+
     if (state == 'start') then
-        love.graphics.setFont(love.graphics.newFont(40))
         love.graphics.printf('Press space to begin.', 200, 280, 600, 'center')
         love.graphics.setLineWidth(6)
         love.graphics.rectangle('line', 200, 200, 600, 300)
         love.graphics.setLineWidth(4)
         love.graphics.rectangle('line', 210, 210, 600 - 20, 300 - 20)
     elseif (state == 'pause') then
-        love.graphics.setFont(love.graphics.newFont(40))
         love.graphics.printf('Game Paused', 200, 200, 600, 'center')
     elseif (state == 'end') then
-        love.graphics.setFont(love.graphics.newFont(40))
         love.graphics.printf('Game Over', 200, 200, 600, 'center')
         love.graphics.printf('Press enter to restart.', 200, 500, 600, 'center')
     end
